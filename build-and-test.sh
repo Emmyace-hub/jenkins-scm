2193a3e9baa21bddab33496f1320f9518311d00b
Emmanuel Adeteye <emmanueladeteye24@gmail.com>
2025-06-11 12:21:37 +0100

    Update build-and-test.sh

--- build-and-test.sh
+++ build-and-test.sh
@@ -1,7 +1,8 @@
-#!/bin/sh
-
+#!/bin/bash
 echo "Building the project..."
+# Add your build commands here, e.g.:
 npm install
 
 echo "Running tests..."
+# Add your test commands here, e.g.:
 npm test
